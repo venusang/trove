053e622b5f98a68b155ccb78e4b2ad9242de994f
venusang <venusang@users.noreply.github.com>
2016-06-15 11:01:28 -0700

    Cleaning up spaces

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,21 @@ var   gulp = require('gulp'),
 	  handlebars = require('gulp-ember-handlebars'),
 	  uglify = require('gulp-uglify'),
 	  concat = require('gulp-concat'),
-    sass = require('gulp-sass');
-    minifyCSS = require('gulp-minify-css');
+	  sass = require('gulp-sass');
+	  minifyCSS = require('gulp-minify-css');
 	  
 // ADD templates in when ready
 gulp.task('default', ['sass', 'scripts','minify-css','watch']);	  
 gulp.task('production', ['css', 'templates', 'scripts']);
 
 gulp.task('sass', function () {
-    gulp.src('resources/sass/troveStyle.scss')
-        .pipe(sass({
-          includePaths: ['resources/sass'],
-          errLogToConsole: true
-        }))
-        .pipe(gulp.dest('resources/css'));
+  gulp.src('resources/sass/troveStyle.scss')
+    .pipe(sass({
+      includePaths: ['resources/sass'],
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest('resources/css'));
 });
-
 
 gulp.task('templates', function() {
   gulp.src([config.templatesPath + '/**/*.hbs'])
@@ -53,8 +52,8 @@ gulp.task('scripts', function() {
   var scriptSrc = [
     'resources/js/libs/jquery-1.10.2.js',
     'resources/js/libs/bootstrap.min.js',
-	'resources/js/libs/handlebars-v1.3.0.js',
-	'resources/js/libs/ember-1.8.0.js',
+    'resources/js/libs/handlebars-v1.3.0.js',
+    'resources/js/libs/ember-1.8.0.js',
     'resources/js/libs/ember-data.min.js',
     'resources/js/libs/select2.min.js',
     'resources/js/libs/jquery.dataTables.min.js',
@@ -82,7 +81,7 @@ gulp.task('watch', function() {
   gulp.watch('resources/js/**/*.js', ['scripts']);
 }); 
 
-  var config = {
-    sassPath: 'resources/sass',
-    templatesPath: 'resources/js/templates'
-  }
+var config = {
+  sassPath: 'resources/sass',
+  templatesPath: 'resources/js/templates'
+}
